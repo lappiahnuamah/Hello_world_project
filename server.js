c855c48b58cf43bc78aae416a52203c88d169eb9
Lawrence Appiah-Nuamah <lappiahnuamahfbc1999@gmail.com>
2024-10-04 19:39:23 +0000

    Worked on Server js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello, this is my first Node js server');
+})
+
+const PORT = 3000;
+server.listen(PORT, ()=>{
+    console.log('Server is running successfully');
+})
+
